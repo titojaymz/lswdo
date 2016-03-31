ea57c12a9bd8f829f92243cf0405f141ceeb03d5
mblejano <mblejano@dswd.gov.ph>
2016-03-31 16:21:08 +0800

    ayaw gumana

--- application/controllers/dashboardc.php
+++ application/controllers/dashboardc.php
@@ -9,10 +9,14 @@ class dashboardc extends CI_Controller {
 
     public function dashboard()
     {
+        $dashboard_model = new dashboard_model();
         $this->load->view('header');
         $this->load->view('nav');
         $this->load->view('sidebar');
-        $this->load->view('dashboard');
+        $this->load->view('dashboard',array('getIndicator'=>$dashboard_model->getFunctionalityScore(),
+            'getRegionName'=>$dashboard_model->getRegions(),
+            'countRegion'=>$dashboard_model->countRegions()
+        ));
         $this->load->view('footer');
     }
 }
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,15 +1,122 @@
+
+
+
 <div class="content">
 
     <div class="container-padding">
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-body table-responsive">
-                    <?php echo $this->session->userdata('user_id'); ?>
+<!--                    --><?php //echo $this->session->userdata('user_id'); ?>
                     <!DOCTYPE HTML>
                     <html>
                     <head>
                         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
                         <title>Highcharts Example</title>
+
+
+<pre>
+
+
+
+
+<?php
+
+//$score = "";
+//$newscore = "";
+//$region_name = "";
+//
+//foreach ($getRegionName as $getRegName):
+//    $region_name .= "'".$getRegName->region_name."',";
+//
+//    foreach ($getIndicator as $getIn):
+//
+//        if($getRegName->region_name == $getIn->region_name)
+//        {
+//
+//            $newscore .= "".$getIn->TotalScore.",";
+////            break;
+//        }
+//        else
+//        {
+//
+//            $newscore .= "0,";
+////            break;
+//        }
+//
+//    endforeach;
+//
+//    $score_format =  substr($newscore,0,-1);
+//
+//endforeach;
+//$region_format =  substr($region_name,0,-1);
+//echo $region_format;
+//echo "<br>";
+//echo "<br>";
+//echo $newscore;
+
+
+
+$score = "";
+$newscore = "";
+$newArray = array();
+foreach($getRegionName  as $getRegName):
+    $arr = $getRegName->region_name;
+    $first = $arr;
+    foreach($getIndicator  as $getIn):
+        if($getRegName->region_name == $getIn->region_name)
+        {
+            $newArray[$first][1] = $getIn->lgu_type_id;
+            $newArray[$first][2] = $getIn->TotalScore;
+            $newArray[$first][3] = $getIn->FinalScore;
+
+        } else {
+            $newArray[$first][1] = "";
+            $newArray[$first][2] = 0;
+            $newArray[$first][3] = 0;
+        }
+    endforeach;
+endforeach;
+
+print_r($newArray);
+/*foreach ($getIndicator as $getIn):
+    $score = "".$getIn->TotalScore."";
+    $region_name ="";
+
+    foreach ($getRegionName as $getRegName):
+
+            if($getRegName->region_name == $getIn->region_name)
+            {
+                $region_name .= "'".$getIn->region_name."',";
+                $newscore .= "".$score.",";
+                break;
+
+            }
+            else
+            {
+                $region_name .= "'".$getRegName->region_name."',";
+                $newscore .= "0,";
+
+
+            }
+
+    endforeach;
+
+    $region_format =  substr($region_name,0,-1);
+
+
+endforeach;
+echo $region_format;
+echo "<br>";
+echo "<br>";
+echo $newscore;
+$score_format =  substr($newscore,0,-1);*/
+
+?>
+
+
+</pre>
+
 
                         <style type="text/css">
                             ${demo.css}
@@ -18,63 +125,51 @@
                             $(function () {
                                 $('#container').highcharts({
                                     chart: {
-                                        type: 'bar'
+                                        type: 'column'
                                     },
                                     title: {
-                                        text: 'SAMPLE DASHBOARD'
+                                        text: 'Level of functionality'
                                     },
                                     subtitle: {
-                                        text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
+                                        text: ''
                                     },
                                     xAxis: {
-                                        categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
-                                        title: {
-                                            text: null
-                                        }
+                                        categories: [<?php echo $region_format;?>
+                                        ],
+                                        crosshair: true
                                     },
                                     yAxis: {
                                         min: 0,
                                         title: {
-                                            text: 'Population (millions)',
-                                            align: 'high'
-                                        },
-                                        labels: {
-                                            overflow: 'justify'
+                                            text: ''
                                         }
                                     },
                                     tooltip: {
-                                        valueSuffix: ' millions'
+                                        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                                        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                                        '<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+                                        footerFormat: '</table>',
+                                        shared: true,
+                                        useHTML: true
                                     },
                                     plotOptions: {
-                                        bar: {
-                                            dataLabels: {
-                                                enabled: true
-                                            }
+                                        column: {
+                                            pointPadding: 0.2,
+                                            borderWidth: 0
                                         }
                                     },
-                                    legend: {
-                                        layout: 'vertical',
-                                        align: 'right',
-                                        verticalAlign: 'top',
-                                        x: -40,
-                                        y: 80,
-                                        floating: true,
-                                        borderWidth: 1,
-                                        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-                                        shadow: true
-                                    },
-                                    credits: {
-                                        enabled: false
-                                    },
                                     series: [{
-                                        name: 'Year 1800',
-                                        data: [107, 31, 635, 203, 2]
+                                        name: 'Fully Functional',
+                                        data: [<?php echo $score_format;?>]
+
                                     }, {
-                                        name: 'Year 1900',
-                                        data: [133, 156, 947, 408, 6]
+                                        name: 'Functional',
+                                        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3, 105.0, 104.3, 91.2, 83.5, 106.6]
+
                                     }, {
-                                        name: 'Year 2012',
-                                        data: [1052, 954, 4250, 740, 38]
+                                        name: 'Partially Functional',
+                                        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2, 105.0, 104.3, 91.2, 83.5, 106.6]
+
                                     }]
                                 });
                             });
@@ -82,10 +177,12 @@
                     </head>
                     <body>
 
-                    <div id="container" style="min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto"></div>
+                    <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
 
                     </body>
                     </html>
+
+
 
                 </div>
             </div>
