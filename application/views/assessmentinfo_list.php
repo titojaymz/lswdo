250375fb20db0b65c8cd505919c982116d8640a2
Mary Grace L. Veniegas <mglveniegas@dswd.gov.ph>
2016-05-13 16:25:18 +0800

    Assessment info list display added record

--- application/views/assessmentinfo_list.php
+++ application/views/assessmentinfo_list.php
@@ -18,7 +18,7 @@
     <div class="page-header">
         <!-- <h1 class="title">Tool for the Assessment of FUNCTIONALITY of LSWDOs</h1>-->
         <ol class="breadcrumb">
-            <li><a href="<?php echo base_url('dashboardc/dashboard'); ?>">Home</a></li>
+            <li><a href="<?php echo base_url('dashboardc/dashboard/'); ?>">Home</a></li>
             <li class="active">Assessment Information</li>
         </ol>
     </div>
