c66dcf1375885ded91cbb2235ee51808b0f890d5
Mary Grace L. Veniegas <mglveniegas@dswd.gov.ph>
2016-05-16 16:18:34 +0800

    Pati budgetallocation list. Thanks po

--- application/models/budgetallocation_model.php
+++ application/models/budgetallocation_model.php
@@ -15,8 +15,7 @@ class budgetallocation_model extends CI_Model {
     public function getBudgetAllocation($profID)
     {
 
-        $sql = 'select a.profile_id,a.sector_id,b.sector_name,a.year_indicated, a.budget_present_year, a.budget_previous_year,
-                a.utilization, a.no_bene_served, a.no_target_bene
+        $sql = 'select *
                 From tbl_lswdo_budget a
                 LEFT JOIN lib_sector b
                 ON a.sector_id = b.sector_id
