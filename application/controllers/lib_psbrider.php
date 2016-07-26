7df9817bc06e1f1b6638a4497fe120ad25cec560
ljesaclayan <ljesaclayan@dswd.gov.ph>
2016-07-26 15:36:02 +0800

    Library PSB Riders \m/

---
+++ application/controllers/lib_psbrider.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ljesaclayan
+ * Date: 7/22/2016
+ * Time: 2:01 PM
+ */
+class lib_psbrider extends CI_Controller {
+
+    public function psblist()
+    {
+        $psbriders = new lib_psbriders_model();
+        $this->load->view('header');
+        $this->load->view('nav');
+        $this->load->view('sidebar');
+        $this->load->view('lib_psbriders_list',array(
+            'PSBMain'=>$psbriders->getPSBmain(),
+            'PSBSub'=>$psbriders->getPSBmainSub(),
+            'psbRidersC'=>$psbriders,
+        ));
+        $this->load->view('footer');
+    }
+
+    public function psbSubCategory($main)
+    {
+        $psbriders = new lib_psbriders_model();
+        $this->load->view('lib_psbriders_subCat', array('psbSubCat' => $psbriders->getPSBsubByMain($main)));
+    }
+
+    public function psbadd()
+    {
+        $psbriders = new lib_psbriders_model();
+        $this->validatePSBRidersIndicator();
+        if (!$this->form_validation->run()) {
+
+            $this->load->view('header');
+            $this->load->view('nav');
+            $this->load->view('sidebar');
+            $this->load->view('lib_psbriders_add', array(
+                'psbMain' => $psbriders->getPSBmain(),
+            ));
+            $this->load->view('footer');
+        } else {
+            $mainCat = $this->input->post('mainCat');
+            $subCategory = $this->input->post('subCategory');
+
+            $addResult = $psbriders->insertPSBRiders($mainCat,$subCategory);
+            if($addResult){
+                $this->redirectIndex();
+
+            }
+        }
+    }
+
+    protected function validatePSBRidersIndicator()
+    {
+        $config = array(
+
+            array(
+                'field'   => 'subCategory',
+                'label'   => 'subCategory',
+                'rules'   => 'required'
+            )
+        );
+
+        return $this->form_validation->set_rules($config);
+
+    }
+    public function redirectIndex()
+    {
+        $page = base_url('lib_psbrider/psblist/');
+//        $sec = "1";
+        header("Location: $page");
+    }
+
+}
